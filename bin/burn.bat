9fe0d177755b81b59e354f5102c31e25494b0aff
vanbwodonk <arif.pens@gmail.com>
2014-09-29 07:46:18 -0700

    Revert "first build success"
    This reverts commit 93c2a69be45b05a1517c7d548d67a3c811fbc69e.

--- bin/burn.bat
+++
@@ -1,9 +0,0 @@
-@ECHO off
-SET FIRMWARE=firmware
-ECHO fixing checksum...........
-fix_checksum %FIRMWARE%.bin
-erase e:firmware.bin
-SLEEP 1
-ECHO replace firmware.bin......
-COPY %FIRMWARE%.bin e:
-SLEEP 1
